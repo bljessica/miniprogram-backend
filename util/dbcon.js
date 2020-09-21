c331e9746c9384619d47ef9fd3896611138f4029
bljessica <1194620082@qq.com>
2020-09-21 08:56:28 +0800

    remove https certification

--- util/dbcon.js
+++ util/dbcon.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+// const url = 'mongodb://root:happy123@127.0.0.1:28018/ques?authSource=admin'
 const url = 'mongodb://127.0.0.1/ques'
 const {TYPE_VALUES, SUBJECT_VALUES, GENDER_VALUES} =  require('../util/const')
 
